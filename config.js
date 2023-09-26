ca5428ae9b92efb05b5c4249d7cd6d86af3e1996
sathviru <78941711+sathviru@users.noreply.github.com>
2023-09-26 19:09:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,22 +6,22 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 global.owner = process.env.OWNER_NUMBER.split(",")
 global.mongodb = process.env.MONGODB_URI || "Enter-MongoURI-HERE"
 global.port= process.env.PORT || 5000
-global.email = 'sam@sec.live'
-global.github = 'https://github.com/prabathLK/PRABATH-MD'
-global.location = 'Sultanpur IN'
-global.gurl = 'https://instagram.com/' // add your username
-global.devs = '94762280384';
-global.website = 'https://github.com/prabathLK/PRABATH-MD' //wa.me/+94762280384
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.imgur.com/H062oEI.jpeg'
+global.email = 'sathviru@gmail.com'
+global.github = 'https://github.com/'
+global.location = ''
+global.gurl = 'https://instagram.com/sathviru_Rupasinghe' // add your username
+global.devs = '94721489840';
+global.website = 'https://github.com/' //wa.me/+94762280384
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.imgur.com/MClOeqe.jpeg'
 module.exports = {
   OPENAI_API_KEY:process.env.OPENAI_API_KEY || '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',
-  botname: process.env.BOT_NAME || 'PRABATH-MD',
+  botname: process.env.BOT_NAME || 'Elisa-MD',
   dl_limit: process.env.DL_SIZE || '300',
   art: process.env.AUTO_RESTART || 'true',
-  sudo: process.env.SUDO || '94762280384',
-  alivepic: process.env.ALIVE_IMG || 'https://telegra.ph/file/ad25b2227fa2a1a01b707.jpg',
-  alivemsj: process.env.ALIVE_MSJ || '*POWERD BY PRABATH-MD* 🎊',
-  ownername:process.env.OWNER_NAME || 'PRABATH',
+  sudo: process.env.SUDO || '94721489840',
+  alivepic: process.env.ALIVE_IMG || 'https://i.imgur.com/MClOeqe.jpeg',
+  alivemsj: process.env.ALIVE_MSJ || '*POWERD BY Elisa-MD* 🎊',
+  ownername:process.env.OWNER_NAME || 'Sathviru',
   sessionName: process.env.SESSION_ID || 'PUT-HERE',
   author: process.env.PACK_INFO.split(";")[0] || 'author', 
   auto_read_status : process.env.AUTO_READ_STATUS || 'false',
